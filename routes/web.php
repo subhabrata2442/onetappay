3753b394b4b552755dd6d97c7e4720675bef4e94
subhabrata2442 <subhabrata2442@gmail.com>
2023-02-03 11:35:02 +0530

    test

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+*/ 
 Route::get('/', 'front\HomeController@index');
 Route::get('index', 'front\HomeController@index');
 Route::get('privacy', 'IndexController@privacy');
